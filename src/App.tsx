9a0ca13f31dc5822072b34d067f90b85919f8205
sw30041995 <sw1225@yandex.ru>
2021-11-27 17:25:38 +0300

    new file

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+    return (
+        <div>
+          This is App component
+          <Rating />
+          <Accordion />
+        </div>
+    );
+}
 
-    </div>
-  );
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+function Star() {
+    return (
+        <div>star</div>
+    );
+}
+
+function Accordion() {
+    return (
+        <div>
+            <h1>Menu</h1>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    );
 }
 
 export default App;
-
-
-
-let a = [];
-a = 3;
-a = 'swsw';
-a = [2,3,4,5];
-let b = {name: 'Ivan'};
-b = {name: ''};
