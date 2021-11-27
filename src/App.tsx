75d2628d6962dca938de9282256ba4119355678e
sw30041995 <sw1225@yandex.ru>
2021-11-27 16:10:11 +0300

    new file

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
+
+
+
+let a = [];
+a = 3;
+a = 'swsw';
+a = [2,3,4,5];
+let b = {name: 'Ivan'};
+b = {name: ''};
